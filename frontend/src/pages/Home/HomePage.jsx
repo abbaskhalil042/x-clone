2d6845bb6dff40e27a62bb1aa167af9d7e04b646
Abbas khalil <khalilabbas389@gmail.com>
2025-04-13 05:13:57 +0530

    tweet card deleted

--- frontend/src/pages/Home/HomePage.jsx
+++ frontend/src/pages/Home/HomePage.jsx
@@ -1,28 +1,7 @@
-// import { Globe } from "./src/components/magicui/globe";
-import { Globe } from "../../components/magicui/globe";
-import React from "react";
-import { Particles } from "../../components/magicui/particles";
-
-import TweetCard from "./TweetCard";
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { BorderBeam } from "../../components/magicui/border-beam";
-import LoginPage from "../Auth/LoginPage";
-
 const HomePage = () => {
   return (
     <div className="h-screen">
       <h1>home page</h1>
-      <TweetCard tweetId="1668408059125702661" />
     </div>
   );
 };
