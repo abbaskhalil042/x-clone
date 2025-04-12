bd0e5baad1bf1524db35e368e7de61c458e2026f
Abbas khalil <khalilabbas389@gmail.com>
2025-04-13 02:42:00 +0530

    final check done

--- backend/controllers/notification.controller.js
+++ backend/controllers/notification.controller.js
@@ -10,7 +10,7 @@ export const getNotifications = async (req, res) => {
 
     await Notification.updateMany({ to: userId }, { $set: { seen: true } });
 
-    res.status(200).json({msg:"success",notification});
+    res.status(200).json({ msg: "success", notification });
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: error.message });
@@ -23,6 +23,33 @@ export const deleteNotifications = async (req, res) => {
     await Notification.deleteMany({ to: userId });
 
     res.status(200).json({ msg: "notifications deleted" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+
+//*optional
+export const deleteNotification = async (req, res) => {
+  try {
+    const notificationId = req.params.id;
+    const userId = req.user._id;
+
+    const notification = await Notification.findByIdAndDelete(notificationId);
+    if (!notification) {
+      return res.status(404).json({ error: "notification not found" });
+    }
+
+    if (notification.to.toString() !== userId.toString()) {
+      return res
+        .status(401)
+        .json({ error: " you are not authorized to delete notification" });
+    }
+
+    await Notification.findByIdAndDelete(notificationId);
+
+    res.status(200).json({ msg: "notification deleted" });
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: error.message });
--- backend/routes/notification.routes.js
+++ backend/routes/notification.routes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { protectedRoute } from "../middleware/auth.js";
-import { deleteNotifications, getNotifications } from "../controllers/notification.controller.js";
+import { deleteNotification, deleteNotifications, getNotifications } from "../controllers/notification.controller.js";
 const notificationRouter = express.Router();
 
 
 notificationRouter.get("/", protectedRoute,getNotifications);
 notificationRouter.delete("/", protectedRoute,deleteNotifications);
+//*optional
+notificationRouter.delete("/:id", protectedRoute,deleteNotification);
 
 
 
