f5cec235075ec3a57ef351dcdb317ba342c40b45
srishtykumari <swty2607@gmail.com>
2025-04-16 15:14:40 +0530

    add profile and edit profile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,17 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/Home/HomePage";
 import LoginPage from "./pages/auth/LoginPage";
 import SignUpPage from "./pages/Auth/SignUpPage";
+<<<<<<< Updated upstream
 import Sidebar from "./pages/Sidebar/Sidebar";
 import Search from "./pages/Search/Search";
+=======
+import Layout from "./pages/Layout";
+import Profile from "./pages/Profile/Profile";
+import Following from "./pages/Followers/Following";
+import Followers from "./pages/Followers/Followers";
+// import Layout from "./Layout"; // Adjust the path if needed
+import ProfileCard from "./pages/Profile/ProfileCard";
+>>>>>>> Stashed changes
 function App() {
   return (
     <div data-theme="abyss">
@@ -11,8 +20,21 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
+<<<<<<< Updated upstream
         <Route path="/sidebar" element={<Sidebar />} />
         <Route path="/search" element={<Search/>} />
+=======
+
+        {/* Pages inside layout */}
+        <Route element={<Layout />}>
+          <Route path="/" element={<HomePage />} />
+          {/* You can add more routes here */}
+          <Route path="/profile" element={<Profile/>} />
+          <Route path="/following" element={<Following/>} />
+          <Route path="/followers" element={<Followers/>} />
+          <Route path="/profileCard" element={<ProfileCard/>} />
+        </Route>
+>>>>>>> Stashed changes
       </Routes>
     </div>
   );
